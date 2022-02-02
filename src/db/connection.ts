be7ecdf2d44036d4f65920aa2c4a02f078447f90
Douglas Hellowell <doog99@gmail.com>
2022-02-02 13:54:23 +0000

    upload

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,17 +2,4 @@ import { PrismaClient } from '@prisma/client';
 const connection = new PrismaClient({ log: ['query'] });
 // can invoke with { log: ['query'] } to log SQL queries as they're made
 
-// connection.user
-//   .findMany({
-//     where: { username: 'tickle122' },
-//     select: {
-//       Comment: true,
-//       Article: true,
-//       avatar_url: true,
-//       name: true,
-//       username: true,
-//     },
-//   })
-//   .then((res) => console.log(JSON.stringify(res, null, 2)));
-
 export default connection;
